e83f8b7ced7666c6153620959a415d97977518b4
G0a19 <Howtoexp30@gmail.com>
2022-05-04 08:41:08 +0300

    update

--- src/javascript/about-me.js
+++ src/javascript/about-me.js
@@ -12,7 +12,7 @@ if ($(window).width() < 992) {
       "#magic",
       0.001,
       { x: "0%", duration: 0.001 },
-      { x: `-${80}%`, duration: 0.001 },
+      { x: `-${120}%`, duration: 0.001 },
       0.001
     )
   );
